7e782d7c55e35f75bdfb7081393ea78f01eb1538
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-09-21 21:14:41 -0600

    cambios form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Form.module.css";
 
 const Form = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!regexEmail.test(email)) {
+      setError("Valid email required");
+    } else {
+      console.log("hola");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <form action="submit" className={style.formContainer}>
+    <form
+      action="submit"
+      className={style.formContainer}
+      onSubmit={handleSubmit}
+    >
       <label>Email address</label>
       <input
         placeholder="email@company.com"
-        type="email"
-        id="email"
+        type="text"
         name="email"
+        onChange={handleOnChange}
+        value={email}
       />
-      <button>Subscribe to monthly newsletter</button>
+      <button type="submit">Subscribe to monthly newsletter</button>
     </form>
   );
 };
